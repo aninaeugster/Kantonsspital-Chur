612b0e9a0cf54e13128cab5762f960c8fcef28ce
Anina Eugster <anina.eugster@mmp.htwchur.ch>
2016-11-21 14:39:35 +0100

    Create home.php

---
+++ home.php
@@ -0,0 +1,100 @@
+
+
+<!-- HTML-Code -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kantonsspital Graubünden - Home</title>
+
+    <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+  </head>
+
+<body>
+  <div class="row">
+    <div class="col-xs-1">
+      <div class="thumbnail">
+        <img src="images/logo.png" alt="LogoKSCH" class="img-responsive">
+      </div><!-- /thumbnail -->
+    </div><!-- /col-sm-2 -->
+  <h1>Willkommen</h1>
+  <!-- Navigation -->
+<nav class="nav navbar navbar-right">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <div class="col-md-12">
+        <div class="thumbnail">
+          <img src="images/default_ks_avatar.png" alt="thumbnailProfilPic" class="img-responsive">
+      <a class="navbar-brand" href="#">Username</a>
+  </div><!-- /thumbnail -->
+</div><!-- /col-sm-2 -->
+</div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class = "col-sm-14">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar navbar-right">
+        <li><a href="#">Home</a></li>
+        <li><a href="profil.php">Profil</a></li>
+        <li><a href="index.php">Logout</a></li>
+      </ul>
+    </div>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav><!-- /Navigation -->
+
+<!-- Hauptinhalt -->
+<div class="container">
+  <div class="row">
+    <div class="col-md-10">
+
+      <!-- Box -->
+      <div class="row">
+          <form  class="form-inline" method="post" action="home.php">
+            <div class="col-xs-8">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <div class="panel-heading text-right">
+                    <small><a class="text-muted" href="#"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></a></small>
+                  </div>
+                  <h3 class="panel-title">Infoblock 1</h3>
+                </div>
+                <div class="panel-body">
+                  <p>Hier folgt der Infotext zur Infobox 1.</p>
+                  <img src="" alt="BildInfobox" class="img-responsive" align="right">
+                </div>
+              </div>
+            </div><!-- /col-sm-10 -->
+          </form>
+        </div> <!-- /Beitrag -->
+    </div> <!-- /Hauptinhalt -->
+
+
+<!-- Javascript -->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script>
+
+</script>
+
+</body>
+</html>
